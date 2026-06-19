3a450defd502eeb821923d37d3aa8b0c4dec579c
Suman Pratihar <pratiharsuman0@gmail.com>
2026-06-20 05:08:14 +0530

    Remove unwanted files

--- suman.py
+++
@@ -1,7 +0,0 @@
-age=int(input("enter your age:  "))
-if (age>18):
-    print("dane")
-elif(age==10):
-     print("interview")
-else:
-     print("you can not drive")     
--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-def factorial_num(num):
-    sol=num*factorial_num(num-1)
-    return sol
-
-print(factorial_num(5))
